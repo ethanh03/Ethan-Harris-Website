ffab7c48fe32e8599898f9f410f4feaff460d026
Ethan Harris <ethan.j.harris3@gmail.com>
2024-09-02 23:01:27 -0500

    fixing background

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Background.css'
-import dark_arrow from '../../assets/dark_arrow.png'
+import dark_arrow from '../assets/dark_arrow.png'
 
 const Background = () => {
   return (
