2fddde3fd6533f4c57661d650e3b753a0be77e4a
Ethan Harris <ethan.j.harris3@gmail.com>
2024-09-02 22:59:45 -0500

    Adding Background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import headshot from './headshot.jpeg';
 import './App.css';
+import Background from './Components/Background';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={headshot} className="App-logo" alt="logo" />
-        <p> Ethan Harris</p>
+      <Background /> 
       </header>
     </div>
   );
---
+++ src/Components/Background.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Background.css'
+import dark_arrow from '../../assets/dark_arrow.png'
+
+const Background = () => {
+  return (
+    <div className='background-container'>
+        <div className="background-text">
+          <h1>Ethan Harris</h1>
+          <p>This site is still under development! I will be updating it regularly :&#41;</p>
+          <p>Contact me: ethan.harris3@utexas.edu</p>
+          <button className='btn'>I want to know more<img src={dark_arrow} alt=''/></button>
+        </div>
+    </div>
+  )
+}
+
+export default Background
